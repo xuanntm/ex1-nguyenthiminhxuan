658ffc032336fe197a8f84762aaa749cbcdddf79
Xuan Nguyen <xuan.ntm92@gmail.com>
2025-02-27 23:01:05 +0800

    Initial commit with checkerboard script

---
+++ checkerboard.py
@@ -0,0 +1,23 @@
+### a simple checkerboard
+import numpy as np
+import matplotlib.pyplot as plt
+def generate_checkerboard(size=64, num_squares=8):
+    """
+    Generates a checkerboard pattern image of size (size x size).
+    """
+    block_size = size // num_squares
+    image = np.zeros((size, size))
+    for i in range(num_squares):
+        for j in range(num_squares):
+            if (i + j) % 2 == 0:
+                image[i * block_size:(i + 1) * block_size, j * block_size:(j + 1) * block_size] = 1
+    return image
+### Generate checkerboard image
+checkerboard = generate_checkerboard()
+
+### Plot checkerboard
+plt.figure(figsize=(6,6))
+plt.imshow(checkerboard, cmap="gray", interpolation="nearest")
+plt.axis("off")
+plt.title("A Checkerboard Pattern")
+plt.show()
